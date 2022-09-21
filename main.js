e3abff66b224ca61f9bfc50a366ec1fde70427a8
Sebastian Pezoa Franzani <sebastianpezoa@gmail.com>
2022-09-21 00:36:53 -0300

    Correccion de CSS y bootstrap

--- main.js
+++ main.js
@@ -70,8 +70,8 @@ function showAssignments() {
                     <p>${assignment.description}</p>
                 </div>
                 <div>
-                    <button onclick="editFunction(this, '${assignment.name}' )">Editar</button>
-                    <button onclick="eraseAssignment(this, '${assignment.name}' )">Borrar</button>
+                    <button class="btn btn-info text-dark" onclick="editFunction(this, '${assignment.name}' )">Editar</button>
+                    <button class="btn btn-danger text-dark" onclick="eraseAssignment(this, '${assignment.name}' )">Borrar</button>
                 </div>
             </article>
       `
